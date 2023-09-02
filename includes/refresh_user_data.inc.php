5a6b79c7c0460585d0c786bd7afc8a5a830b7e56
Morgan Daniel! <morganjdaniel@gmail.com>
2023-09-02 23:37:24 +0100

    Update refresh_user_data.inc.php

--- includes/refresh_user_data.inc.php
+++ includes/refresh_user_data.inc.php
@@ -138,7 +138,7 @@ foreach($units as $unit){
         $potentcy = $stats[17];
         $tenacity = $stats[18];
 
-        echo "Speed " . $speed;
+        // echo "Speed " . $speed;
 
         $stats = json_encode($stats);
 
@@ -152,9 +152,9 @@ foreach($units as $unit){
                     '$speed','$health','$protection','$physical_damage',
                     '$special_damage','$potentcy','$tenacity','$equiped','$legend','$ultimate',
                     '$alignment')";
-        // echo $sql_c;
+        echo '<div>' . $sql_c . '</div></br>';
         $result_c = $conn->query($sql_c);
-        print_r($result_c);
+        // print_r($result_c);
         if ($gear == 13){
             $total_gear += 1;
         }
